e44a4c273a0e255a988bed5eb1c48bf5b18bb71b
Pavel <kets911@users.noreply.github.com>
2018-02-28 00:24:13 +0300

    regrouped tests, added recyclerView test.

--- app/src/androidTest/java/ru/example/kets/funnyapp/ShowMessageActivityTest.java
+++ app/src/androidTest/java/ru/example/kets/funnyapp/ShowMessageActivityTest.java
@@ -1,13 +1,21 @@
 package ru.example.kets.funnyapp;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -24,8 +32,29 @@ public class ShowMessageActivityTest {
     @Test
     public void recyclerViewIsDisplayed(){
 
-//        onView(withId(R.id.cardView)).check(matches(not(isDisplayed())));
+//        onView(withId(R.id.cardView)).check(matches(isDisplayed()));
 //        onView(withId(R.id.textView)).check(matches(not(isDisplayed())));
         onView(withId(R.id.recycleList)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void deletMessageTest(){
+        onView(withId(R.id.recycleList)).perform(RecyclerViewActions.actionOnItemAtPosition(1, new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return null;
+            }
+
+            @Override
+            public String getDescription() {
+                return "Click on button";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                View v = view.findViewById(R.id.btnDelet);
+                v.performClick();
+            }
+        }));
+
     }
 }
